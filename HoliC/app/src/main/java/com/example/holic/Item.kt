b2bd7ae9057fda5dd1cd89883e6f70640fc66bf8
yeawonlee <wwon525@naver.com>
2019-12-07 18:52:30 +0900

    delete project

--- HoliC/app/src/main/java/com/example/holic/Item.kt
+++
@@ -1,6 +0,0 @@
-package com.example.holic
-
-data class Item (
-    var date : String? = null,
-    var image : Int? = 0
-)
--- HoliC/app/src/main/java/com/example/holic/basecalendar/ViewHolderHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.holic.basecalendar
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.extensions.LayoutContainer
-
-open class ViewHolderHelper(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
